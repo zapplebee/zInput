8dee7dc3aa3d1a8375f5146ccdbb0b2b3673c0e3
Zachary Skalko <zskalko@gmail.com>
2014-06-14 18:31:31 -0500

    Moved some CSS
    moved the CSS to vertically center the text in the button to inline.
    Reduced the character count.

--- zRadio.js
+++ zRadio.js
@@ -13,7 +13,7 @@ $.each(inputNames, function(index,value){
 
 	var $radios = $("input[name='" + value + "']");
 	$radios.wrapAll('<div class="zRadioWrapper" />');
-	$radios.wrap(function(){ return '<div class="zRadio"><span class="zRadioInner"><span class="zRadioInnermost">' + $(this).attr("title") + '</span></span></div>'});
+	$radios.wrap(function(){ return '<div class="zRadio"><span style="display:table;width: 100%;height: 100%;"><span style="display: table-cell;vertical-align:middle;">' + $(this).attr("title") + '</span></span></div>'});
 	});
 
 
@@ -22,7 +22,8 @@ var $zRadio = $(".zRadio");
 $zRadio.click(function(){
 	$theClickedButton = $(this);
 	$theClickedButton.find(":radio").prop("checked", true);
-	$theClickedButton.parent(".zRadioWrapper").children().removeClass("zSelected");
+	//move up the DOM to the .zRadioWrapper and then select children. Remove .zSelected from all .zRadio
+	$theClickedButton.parent().children().removeClass("zSelected");
 	$theClickedButton.addClass("zSelected");	
 	});
 }
