3114e595dfb6eaf190ea9d3f44f317538f8bc069
Zachary Skalko <zskalko@gmail.com>
2014-06-14 20:46:02 -0500

    Added functionality to include checkboxes
    Checkboxes are unwrapped and have a hacky CSS to fix the right margin.

---
+++ zInput.js
@@ -0,0 +1,49 @@
+$.fn.zInput = function(){
+
+var $inputs = this.find(":radio,:checkbox");
+$inputs.hide();
+var inputNames = [];
+$inputs.map(function(){
+  inputNames.push($(this).attr('name'));
+});
+
+inputNames = $.unique(inputNames);
+
+$.each(inputNames, function(index,value){
+
+	var $element = $("input[name='" + value + "']");
+	var elementType = $element.attr("type");
+	if (elementType == "radio"){
+		$element.wrapAll('<div class="zRadioWrapper" />');
+		$element.wrap(function(){ return '<div class="zInput"><span style="display:table;width: 100%;height: 100%;"><span style="display: table-cell;vertical-align:middle;">' + $(this).attr("title") + '</span></span></div>'});
+	}
+	if (elementType == "checkbox")
+	{
+		$element.wrap(function(){ return '<div class="zInput zCheckbox"><span style="display:table;width: 100%;height: 100%;"><span style="display: table-cell;vertical-align:middle;">' + $(this).attr("title") + '</span></span></div>'});	
+	}
+	
+	});
+
+
+var $zRadio = $(".zInput").not(".zCheckbox");
+var $zCheckbox	= $(".zCheckbox");
+
+$zRadio.click(function(){
+	$theClickedButton = $(this);
+
+	//move up the DOM to the .zRadioWrapper and then select children. Remove .zSelected from all .zRadio
+	$theClickedButton.parent().children().removeClass("zSelected");
+	$theClickedButton.addClass("zSelected");
+	$theClickedButton.find(":radio").prop("checked", true);	
+	});
+
+$zCheckbox.click(function(){
+	$theClickedButton = $(this);
+
+	//move up the DOM to the .zRadioWrapper and then select children. Remove .zSelected from all .zRadio
+	$theClickedButton.toggleClass("zSelected");
+	$theClickedButton.find(':checkbox').each(function () { this.checked = !this.checked; });
+	});	
+	
+}
+
