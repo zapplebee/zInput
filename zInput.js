55c0907027edaa787272e1d82d92f29262965dff
Zachary Skalko <zskalko@gmail.com>
2014-06-14 21:31:36 -0500

    gunky css

--- zInput.js
+++ zInput.js
@@ -13,7 +13,7 @@ $.each(inputNames, function(index,value){
 
 	var $element = $("input[name='" + value + "']");
 	var elementType = $element.attr("type");
-	$element.wrapAll('<div class="zRadioWrapper" />');
+	$element.wrapAll('<div class="zInputWrapper" />');
 	if (elementType == "radio"){
 		$element.wrap(function(){ return '<div class="zInput"><span style="display:table;width: 100%;height: 100%;"><span style="display: table-cell;vertical-align:middle;">' + $(this).attr("title") + '</span></span></div>'});
 	}
